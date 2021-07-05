51309ab55e98f313c01cb970da669d163ee276e5
Mujeeb <mujeebmjrhaman1027@gmial.com>
2021-07-05 14:11:33 +0200

    added the sign in button

--- lib/views/sign_in.dart
+++ lib/views/sign_in.dart
@@ -13,7 +13,8 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60.0), child: appBarMain(context),
+        preferredSize: Size.fromHeight(60.0),
+        child: appBarMain(context),
       ),
       body: Container(
         child: Padding(
@@ -21,18 +22,46 @@ class _SignInState extends State<SignIn> {
           child: Column(
             children: [
               TextField(
-                style: simpleTextStyle(),
-                decoration: textFieldInputDecoration("email")
-              ),
+                  style: simpleTextStyle(),
+                  decoration: textFieldInputDecoration("email")),
               TextField(
                   style: simpleTextStyle(),
-                  decoration: textFieldInputDecoration("password")
+                  decoration: textFieldInputDecoration("password")),
+              SizedBox(
+                height: 8.0,
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Text(
+                  "Forgot Password?",
+                  style: simpleTextStyle(),
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(
+                      colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC)
+                      ],
+                    )),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "Sign In",
+                  style: biggerTextStyle(),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ],
           ),
         ),
       ),
-
     );
   }
 }
