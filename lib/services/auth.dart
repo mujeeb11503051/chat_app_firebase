109a4934a2a125f7fb523bd30c631eb9a76299ef
Mujeeb <mujeebmjrhaman1027@gmial.com>
2021-07-10 13:57:11 +0200

    password reset and sign out

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,36 +1,54 @@
 import 'package:chat_app_firebase/model/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthMethods{
-
+class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-
-
-  UserM? _userFromFirebaseUser(User user){
-    return  UserM(userId: user.uid);
+  UserM? _userFromFirebaseUser(User user) {
+    return UserM(userId: user.uid);
   }
 
   //sign in with email and password
-  Future signInWithEmailAndPassword(String email, String password) async{
-    try{
-      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
       User? firebaseUser = result.user;
       return _userFromFirebaseUser(firebaseUser!);
-    }catch(e){
+    } catch (e) {
       print(e.toString());
     }
   }
 
   //sign up with emil and password
-  Future signUpWithEmailAndPassword(String email, String password) async{
-    try{
-      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  Future signUpWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
       User? firebaseUser = result.user;
       return _userFromFirebaseUser(firebaseUser!);
-    }catch(e){
+    } catch (e) {
       print(e.toString());
     }
   }
 
+  //resting password
+  Future resetPass(String email) async {
+    try {
+      return await _auth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  //signout
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }
